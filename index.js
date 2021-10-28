d586cd5dd73c3e99162ada03525858e867fa2066
Wadson Almeida <wadson.dev92@gmail.com>
2021-10-28 18:38:04 -0300

    Requisito 1 finalmente

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +11,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res, _next) => {
+  try {
+    const response = fs.readFileSync('./talker.json');
+    res.status(HTTP_OK_STATUS).send(JSON.parse(response));
+  } catch (err) {
+    console.log('Erro', err.message);
+  }
 });
 
 app.listen(PORT, () => {

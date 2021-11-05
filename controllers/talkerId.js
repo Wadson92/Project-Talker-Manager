b47ff5b498569706fc566dd899ae89f9bb733417
Wadson Almeida <wadson.dev92@gmail.com>
2021-11-04 22:34:24 -0300

    Requisito 02 ok, com a ajuda da Carol Santos e Bruno Augusto

--- controllers/talkerId.js
+++ controllers/talkerId.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 const HTTP_OK_STATUS = 200;
-const talkerJson = './talker.json';
 const NOT_FOUND = {
   status: 404,
   message: 'Pessoa palestrante não encontrada',
@@ -9,15 +8,16 @@ const NOT_FOUND = {
 
 module.exports = (req, res, next) => {
   try {
+    const talkerJson = './talker.json';
     const { id } = req.params;
     const response = fs.readFileSync(talkerJson);
     const allTalkers = JSON.parse(response);
-    const talker = allTalkers.find((talk) => +talk.id === +id);
+    const talker = allTalkers.find((talk) => talk.id === parseInt(id, 10));
     if (!talker) {
       return next(NOT_FOUND);
     }
-    res.status(HTTP_OK_STATUS).json(talker);
-  } catch (erro) {
-    console.log(erro);
+    return res.status(HTTP_OK_STATUS).json(talker);
+  } catch (error) {
+    console.log(error.message);
   }
 };
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,3 @@
 module.exports = (err, _req, res, _next) => {
-  console.log('caiu aqui');
-  res.status(400).send({ message: err.message });
+  res.status(err.status).send({ message: err.message });
 };
